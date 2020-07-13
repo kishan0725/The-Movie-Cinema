2b3fe615e835f347172d6b2faa1dea5d286d63d5
kishan0725 <kishansmart0@gmail.com>
2020-07-13 11:47:41 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# The-Movie-Cinema
+
+![Python](https://img.shields.io/badge/Python-3.8-blueviolet)
+![Framework](https://img.shields.io/badge/Framework-Flask-red)
+![Frontend](https://img.shields.io/badge/Frontend-HTML/CSS/JS-green)
+![API](https://img.shields.io/badge/API-TMDB-fcba03)
+
+This application provides all the details of the requested movie such as overview, genre, release date, rating, runtime, top cast, reviews, recommended movies, etc.
+
+The details of the movies(title, genre, runtime, rating, poster, etc) are fetched using an API by TMDB, https://www.themoviedb.org/documentation/api, and using the IMDB id of the movie in the API, I did web scraping to get the reviews given by the user in the IMDB site using `beautifulsoup4` and performed sentiment analysis on those reviews.
+
+Check out the live demo: https://the-movie-cinema.herokuapp.com/
+
+## How to get the API key?
+
+Create an account in https://www.themoviedb.org/, click on the `API` link from the left hand sidebar in your account settings and fill all the details to apply for API key. You will see the API key in your `API` sidebar once your request is approved.
+
+## How to run the project?
+
+1. Install all the libraries mentioned in the [requirements.txt](https://github.com/kishan0725/Movie-Recommendation-System-with-Sentiment-Analysis/blob/master/requirements.txt) file.
+2. Clone this repository in your local system.
+3. Replace YOUR_API_KEY in the `static/recommend.js` file.
+4. Open the command prompt from your project directory and run the command `python main.py`.
+5. Go to your browser and type `http://127.0.0.1:5000/` in the address bar.
+6. Hurray! That's it.
+
+### Sources of the datasets 
+
+1. [IMDB 5000 Movie Dataset](https://www.kaggle.com/carolzhangdc/imdb-5000-movie-dataset)
+2. [The Movies Dataset](https://www.kaggle.com/rounakbanik/the-movies-dataset)
+3. [List of movies in 2018](https://en.wikipedia.org/wiki/List_of_American_films_of_2018)
+4. [List of movies in 2019](https://en.wikipedia.org/wiki/List_of_American_films_of_2019)
+5. [List of movies in 2020](https://en.wikipedia.org/wiki/List_of_American_films_of_2020)
+
---
+++ static/recommend.js
@@ -0,0 +1,242 @@
+$(function() {
+  // Button will be disabled until we type anything inside the input field
+  const source = document.getElementById('autoComplete');
+  const inputHandler = function(e) {
+    if(e.target.value==""){
+      $('.movie-button').attr('disabled', true);
+    }
+    else{
+      $('.movie-button').attr('disabled', false);
+    }
+  }
+  source.addEventListener('input', inputHandler);
+
+  $('.movie-button').on('click',function(){
+    var my_api_key = 'YOUR_API_KEY';
+    var title = $('.movie').val();
+    if (title=="") {
+      $('.results').css('display','none');
+      $('.fail').css('display','block');
+    }
+    else{
+      load_details(my_api_key,title);
+    }
+  });
+});
+
+// will be invoked when clicking on the recommended movies
+function recommendcard(e){
+  var my_api_key = 'YOUR_API_KEY';
+  var title = e.getAttribute('title'); 
+  load_details(my_api_key,title);
+}
+
+
+// get the basic details of the movie from the API (based on the name of the movie)
+function load_details(my_api_key,title){
+  $.ajax({
+    type: 'GET',
+    url:'https://api.themoviedb.org/3/search/movie?api_key='+my_api_key+'&query='+title,
+
+    success: function(movie){
+      if(movie.results.length<1){
+        $('.fail').css('display','block');
+        $('.results').css('display','none');
+        $("#loader").delay(500).fadeOut();
+      }
+      else{
+        $("#loader").fadeIn();
+        $('.fail').css('display','none');
+        $('.results').delay(1000).css('display','block');
+        var movie_id = movie.results[0].id;
+        var movie_title = movie.results[0].title;
+        var movie_title_org = movie.results[0].original_title;
+        get_movie_details(movie_id,my_api_key,movie_title,movie_title_org);
+      }
+    },
+    error: function(){
+      alert('Invalid Request');
+      $("#loader").delay(500).fadeOut();
+    },
+  });
+}
+
+// get all the details of the movie using the movie id.
+function get_movie_details(movie_id,my_api_key,movie_title,movie_title_org) {
+  $.ajax({
+    type:'GET',
+    url:'https://api.themoviedb.org/3/movie/'+movie_id+'?api_key='+my_api_key,
+    success: function(movie_details){
+      show_details(movie_details,movie_title,my_api_key,movie_id,movie_title_org);
+    },
+    error: function(){
+      alert("API Error!");
+      $("#loader").delay(500).fadeOut();
+    },
+  });
+}
+
+// passing all the details to python's flask for displaying and scraping the movie reviews using imdb id
+function show_details(movie_details,movie_title,my_api_key,movie_id,movie_title_org){
+  var imdb_id = movie_details.imdb_id;
+  var poster;
+  if(movie_details.poster_path){
+    poster = 'https://image.tmdb.org/t/p/original'+movie_details.poster_path;
+  }
+  else {
+    poster = 'static/default.jpg';
+  }
+  var overview = movie_details.overview;
+  var genres = movie_details.genres;
+  var rating = movie_details.vote_average;
+  var vote_count = movie_details.vote_count;
+  var release_date = movie_details.release_date;
+  var runtime = parseInt(movie_details.runtime);
+  var status = movie_details.status;
+  var genre_list = []
+  for (var genre in genres){
+    genre_list.push(genres[genre].name);
+  }
+  var my_genre = genre_list.join(", ");
+  if(runtime%60==0){
+    runtime = Math.floor(runtime/60)+" hour(s)"
+  }
+  else {
+    runtime = Math.floor(runtime/60)+" hour(s) "+(runtime%60)+" min(s)"
+  }
+  
+  movie_cast = get_movie_cast(movie_id,my_api_key);
+  
+  ind_cast = get_individual_cast(movie_cast,my_api_key);
+
+  recommendations = get_recommendations(movie_id, my_api_key);
+  
+  details = {
+      'title':movie_title,
+      'cast_ids':JSON.stringify(movie_cast.cast_ids),
+      'cast_names':JSON.stringify(movie_cast.cast_names),
+      'cast_chars':JSON.stringify(movie_cast.cast_chars),
+      'cast_profiles':JSON.stringify(movie_cast.cast_profiles),
+      'cast_bdays':JSON.stringify(ind_cast.cast_bdays),
+      'cast_bios':JSON.stringify(ind_cast.cast_bios),
+      'cast_places':JSON.stringify(ind_cast.cast_places),
+      'imdb_id':imdb_id,
+      'poster':poster,
+      'genres':my_genre,
+      'overview':overview,
+      'rating':rating,
+      'vote_count':vote_count.toLocaleString(),
+      'rel_date':release_date,  
+      'release_date':new Date(release_date).toDateString().split(' ').slice(1).join(' '),
+      'runtime':runtime,
+      'status':status,
+      'rec_movies':JSON.stringify(recommendations.rec_movies),
+      'rec_posters':JSON.stringify(recommendations.rec_posters),
+      'rec_movies_org':JSON.stringify(recommendations.rec_movies_org)
+  }
+
+  $.ajax({
+    type:'POST',
+    data:details,
+    url:"/recommend",
+    dataType: 'html',
+    complete: function(){
+      $("#loader").delay(500).fadeOut();
+    },
+    success: function(response) {
+      $('.results').html(response);
+      $('#autoComplete').val('');
+      $(window).scrollTop(0);
+    }
+  });
+}
+
+// get the details of individual cast
+function get_individual_cast(movie_cast,my_api_key) {
+    cast_bdays = [];
+    cast_bios = [];
+    cast_places = [];
+    for(var cast_id in movie_cast.cast_ids){
+      $.ajax({
+        type:'GET',
+        url:'https://api.themoviedb.org/3/person/'+movie_cast.cast_ids[cast_id]+'?api_key='+my_api_key,
+        async:false,
+        success: function(cast_details){
+          cast_bdays.push((new Date(cast_details.birthday)).toDateString().split(' ').slice(1).join(' '));
+          cast_bios.push(cast_details.biography);
+          cast_places.push(cast_details.place_of_birth);
+        }
+      });
+    }
+    return {cast_bdays:cast_bdays,cast_bios:cast_bios,cast_places:cast_places};
+  }
+
+// getting the details of the cast for the requested movie
+function get_movie_cast(movie_id,my_api_key){
+    cast_ids= [];
+    cast_names = [];
+    cast_chars = [];
+    cast_profiles = [];
+
+    top_10 = [0,1,2,3,4,5,6,7,8,9];
+    $.ajax({
+      type:'GET',
+      url:"https://api.themoviedb.org/3/movie/"+movie_id+"/credits?api_key="+my_api_key,
+      async:false,
+      success: function(my_movie){
+        if(my_movie.cast.length>=10){
+          top_cast = [0,1,2,3,4,5,6,7,8,9];
+        }
+        else {
+          top_cast = [0,1,2,3,4];
+        }
+        for(var my_cast in top_cast){
+          cast_ids.push(my_movie.cast[my_cast].id)
+          cast_names.push(my_movie.cast[my_cast].name);
+          cast_chars.push(my_movie.cast[my_cast].character);
+          if(my_movie.cast[my_cast].profile_path){
+            cast_profiles.push("https://image.tmdb.org/t/p/original"+my_movie.cast[my_cast].profile_path);
+          }
+          else {
+            cast_profiles.push("static/default.jpg");
+          }
+        }
+      },
+      error: function(){
+        alert("Invalid Request!");
+        $("#loader").delay(500).fadeOut();
+      }
+    });
+
+    return {cast_ids:cast_ids,cast_names:cast_names,cast_chars:cast_chars,cast_profiles:cast_profiles};
+  }
+
+  // getting recommendations
+  function get_recommendations(movie_id, my_api_key) {
+    rec_movies = [];
+    rec_posters = [];
+    rec_movies_org = [];
+
+    $.ajax({
+      type: 'GET',
+      url: "https://api.themoviedb.org/3/movie/"+movie_id+"/recommendations?api_key="+my_api_key,
+      async: false,
+      success: function(recommend) {
+        for(var recs in recommend.results) {
+          rec_movies.push(recommend.results[recs].title);
+          rec_movies_org.push(recommend.results[recs].original_title);
+          if(recommend.results[recs].poster_path){
+            rec_posters.push("https://image.tmdb.org/t/p/original"+recommend.results[recs].poster_path);
+          }
+          else {
+            rec_posters.push("static/default.jpg");
+          }
+        }
+      },
+      error: function() {
+        alert("Invalid Request!");
+        $("#loader").delay(500).fadeOut();
+      }
+    });
+    return {rec_movies:rec_movies,rec_movies_org:rec_movies_org,rec_posters:rec_posters};
+  }
