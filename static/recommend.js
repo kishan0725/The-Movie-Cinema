2cbc31ad57926fd064ac07a344e6f34f7901b2e3
Piyush Agrawal <71832583+piyushagrawal1403@users.noreply.github.com>
2023-06-16 17:45:10 +0530

    Update recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -1,5 +1,5 @@
 // Replace 'YOUR_API_KEY' below with your API key retrieved from https://www.themoviedb.org
-var myAPI = 'YOUR_API_KEY'  // global string to be consistent with future usages elsewhere
+var myAPI = '8265bd1679663a7ea12ac168da84d2e8'  // global string to be consistent with future usages elsewhere
 $(function() {
   $('#movie_list').css('display','none');
   $('#autoComplete').blur(function() {
